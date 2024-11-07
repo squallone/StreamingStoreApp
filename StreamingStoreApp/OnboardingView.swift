8109c66be85e987a69c7049bb2e3d11b9dbe47c6
Abdiel Soto <abdielsba@gmail.com>
2024-11-07 02:06:40 -0600

    Add fonts and create the Onboarding view

---
+++ StreamingStoreApp/OnboardingView.swift
@@ -0,0 +1,18 @@
+//
+//  OnboardingView.swift
+//  StreamingStoreApp
+//
+//  Created by Abdiel Soto on 07/11/24.
+//
+
+import SwiftUI
+
+struct OnboardingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OnboardingView()
+}
