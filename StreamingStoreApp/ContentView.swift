26cb9e75739085591a61f45c6026fbe7d1563c09
Abdiel Soto <abdielsba@gmail.com>
2024-11-07 01:41:30 -0600

    Initial project

---
+++ StreamingStoreApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StreamingStoreApp
+//
+//  Created by Abdiel Soto on 07/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ StreamingStoreApp/StreamingStoreAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StreamingStoreAppApp.swift
+//  StreamingStoreApp
+//
+//  Created by Abdiel Soto on 07/11/24.
+//
+
+import SwiftUI
+
+@main
+struct StreamingStoreAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
